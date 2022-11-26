63bf2ddf2883bb2939744f81cc5a5e9716965af5
Aiziji <aletv1501@gmail.com>
2022-11-26 18:57:19 -0400

    Add validations for models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :bike
+
+  validates :date,:location, bike_id, user_id presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :reservations
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, uniqueness: true, on: :create
 end
