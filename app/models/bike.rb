24e2c20e7341a6718fecca9e4fce4d11ff3e309d
jevazquezb <je.vazquezb@gmail.com>
2022-12-06 14:21:06 -0600

    Add dependant destroy to the bike model

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,5 +1,5 @@
 class Bike < ApplicationRecord
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 
   validates :brand, :model, :photo, :power, :weight, :price, :description, presence: true
 end
